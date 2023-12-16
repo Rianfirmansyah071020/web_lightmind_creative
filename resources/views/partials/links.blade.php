2d24dbdeffd678940afd88207d996856b2c62a0a
Rianfirmansyah071020 <rian071020@gmail.com>
2023-12-16 21:12:55 +0700

    navbar

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,3 +1,12 @@
 <script src="{{ asset('assets/jquery/jquery.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/sass/main.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+    href="https://fonts.googleapis.com/css2?family=Lobster+Two:wght@700&family=Mitr:wght@500&family=Oleo+Script:wght@700&display=swap"
+    rel="stylesheet">
--- resources/views/partials/navbar_desktop.blade.php
+++ resources/views/partials/navbar_desktop.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+<div class="informasi-singkat">
+    <div class="container">
+        <p class="text-start"><i class="fa-brands fa-whatsapp"></i> : 0853-6406-0618</p>
+    </div>
 </div>
+<nav class="navbar navbar-expand-lg navbar-custom p-4 d-none d-sm-none d-lg-block d-md-none d-xl-block">
+    <div class="container-fluid">
+        <a class="navbar-brand text-brand" href="#">{{ env('APP_NAME') }}</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-link-navbar active" aria-current="page" href="#">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-link-navbar" href="#">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-link-navbar" href="#">Tim</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-link-navbar" href="#">Proyek</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
